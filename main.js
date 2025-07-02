ddcb56e259c42d4a5600ace9af2893953dcad845
Gabriel Bento <bielnovaes61@gmail.com>
2025-07-02 15:26:06 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function abrirPorta (tipo) {
+    if (tipo === 'porta1') {
+        alert("Ops, você encontrou um monstro!");
+    }
+    else if (tipo === 'porta2') {
+        alert("Você caiu em uma armadilha.");
+    }
+    else {
+        alert("Parabéns, você encontrou uma saida.");
+    }
+
+}
